d90c7719e0797342c54968b3e0008fe3ee2e6e18
Carol Wargo <carolwargo@gmail.com>
2025-03-30 11:35:45 -0400

    update base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/Homepage/Homepage';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -30,31 +30,25 @@ function App() {
   }, [token, userId]);
 
   return (
-  
-      <div className="App">
-          <BrowserRouter basename='/recruit'>
+    <div className="App">
+    <Router basename='/recruit'>  
+
         <Navbar token={token} setToken={setToken} />
         <Routes>
           <Route path="/about" element={<AboutPage />} />
-          {/* Root: Redirect based on auth */}
           <Route
             path="/"
             element={token ? <Navigate to={`/intake/${userId}`} /> : <HomePage token={token} setToken={setToken} />}
           />
-          {/* Auth Page */}
           <Route path="/auth" element={<AuthPage token={token} setToken={setToken} setUserId={setUserId} />} />
-
-          {/* Protected Intake Routes */}
           <Route element={<ProtectedRoute token={token} />}>
             <Route path="/intake/:userId" element={<IntakePage token={token} userId={userId} />}>
-              <Route index element={<Navigate to="personal" replace />} /> {/* Default to personal */}
+              <Route index element={<Navigate to="personal" replace />} />
               <Route path="personal" element={<PersonalForm token={token} userId={userId} />} />
               <Route path="contact" element={<ContactForm token={token} userId={userId} />} />
               <Route path="athletic" element={<AthleticForm token={token} userId={userId} />} />
               <Route path="academic" element={<AcademicForm token={token} userId={userId} />} />
             </Route>
-
-            {/* Settings Routes */}
             <Route path="/settings/*" element={<UserSettings token={token} userId={userId} />}>
               <Route index element={<div>User Settings Dashboard (TBD)</div>} />
               <Route path="personal" element={<div>Personal Info (TBD)</div>} />
@@ -62,12 +56,10 @@ function App() {
               <Route path="academic" element={<div>Academic Profile (TBD)</div>} />
             </Route>
           </Route>
-
-          {/* Catch-all for unmatched routes */}
           <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
-        </BrowserRouter>
-      </div>
+    </Router>
+    </div>
   );
 }
 
